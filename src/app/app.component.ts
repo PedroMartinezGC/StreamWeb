d40337da020ce299a28d2281c107de5d675b6f93
PedroMartinezGC <97804671+PedroMartinezGC@users.noreply.github.com>
2022-02-09 18:10:36 +0100

    arrow-movement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public title = 'StreamWeb';
 
+
   constructor(){
+  }
+  ngOnInit(){
+  	$("#logo-profile").mouseover(function(){
+  		$("#logo-arrow").css("-moz-transform", "rotate(180deg)")
+  						.css("-ms-transform", "rotate(180deg)")
+  						.css("-o-transform", "rotate(180deg)")
+  						.css("-webkit-transform", "rotate(180deg)");
+  	});
+  	$("#logo-profile").mouseout(function(){
+  		$("#logo-arrow").css("-moz-transform", "rotate(0deg)")
+  						.css("-ms-transform", "rotate(0deg)")
+  						.css("-o-transform", "rotate(0deg)")
+  						.css("-webkit-transform", "rotate(0deg)");
+  	});
   }
 
 
