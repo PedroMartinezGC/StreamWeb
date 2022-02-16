9e2ef720c73a1df9895ed02b66a5c0572feedadb
PedroMartinezGC <97804671+PedroMartinezGC@users.noreply.github.com>
2022-02-16 01:21:11 +0100

    images- toprated

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -2,5 +2,5 @@
 export var Global = {
 	url: "https://api.themoviedb.org/3",
   	api_key: "api_key=d62e4f292b14deffe10ed43720cf1414",
-  	url_img: "https://image.tmdb.org/t/p/w500"
+  	url_img: "https://image.tmdb.org/t/p/original"
 }
